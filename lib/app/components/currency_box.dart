7a28ff050a6f5cce9e1e84d0b911b21d8e0fe10d
Viktor <viktoorrocha@gmail.com>
2021-11-10 23:27:44 -0400

    Iniciando controllers

---
+++ lib/app/components/currency_box.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class CurrencyBox extends StatelessWidget {
+  const CurrencyBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+            flex: 1,
+            child: SizedBox(
+              height: 56.5,
+              child: DropdownButton(
+                isExpanded: true,
+                underline: Container(
+                  height: 1,
+                  color: Colors.amber,
+                ),
+                items: [
+                  DropdownMenuItem(
+                    child: Text('Real'),
+                  ),
+                ],
+                onChanged: null,
+              ),
+            )),
+        SizedBox(
+          width: 10,
+        ),
+        Expanded(
+            flex: 2,
+            child: TextField(
+              decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.amber))),
+            ))
+      ],
+    );
+  }
+}
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:moedas/app/components/currency_box.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -6,10 +7,34 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Image.asset(name)
-        ],
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding:
+              const EdgeInsets.only(top: 100, right: 30, left: 30, bottom: 20),
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/dinheiro.png',
+                width: 150,
+                height: 150,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              CurrencyBox(),
+              SizedBox(
+                height: 10,
+              ),
+              CurrencyBox(),
+              SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(onPressed: () {}, child: Text('Converter'))
+            ],
+          ),
+        ),
       ),
     );
   }
